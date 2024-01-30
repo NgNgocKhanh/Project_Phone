c189739d06d3240b5f72a41d9dac51107f4ef54f
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-01-30 19:37:07 +0700

    phuong_exit_button_windows_application

--- SmartPhone/src/main/java/com/example/smartphone/RegisterController.java
+++ SmartPhone/src/main/java/com/example/smartphone/RegisterController.java
@@ -1,6 +1,9 @@
 package com.example.smartphone;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class RegisterController {
@@ -10,5 +13,15 @@ public class RegisterController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    private Button exitButton;
+
+    // ...
+
+    @FXML
+    private void handleExitButton(ActionEvent event) {
+        // Gọi phương thức exit() khi nút Exit được nhấn
+        Platform.exit();
     }
 }
