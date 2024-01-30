f60005f6837c150e83cffd40c6fb8e0e0fd19073
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-01-30 19:24:34 +0700

    phuong_switch_login_to_register

---
+++ SmartPhone/src/main/java/com/example/smartphone/SceneController.java
@@ -0,0 +1,29 @@
+package com.example.smartphone;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void SwitchToRegister(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("register.fxml"));
+            root = fxmlLoader.load();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
