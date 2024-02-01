be8c9b5155297e6a72e27fda875c2a0ed250fed0
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-02-01 19:42:13 +0700

    phuong_check_login_success

--- SmartPhone/src/main/java/com/example/smartphone/LoginController.java
+++ SmartPhone/src/main/java/com/example/smartphone/LoginController.java
@@ -1,14 +1,24 @@
 package com.example.smartphone;
 
+import dao.JDBCConnect;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class LoginController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class LoginController{
     @FXML
     private Label welcomeText;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
 }
