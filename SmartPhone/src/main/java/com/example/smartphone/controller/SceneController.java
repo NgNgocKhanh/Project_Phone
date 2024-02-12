dd5e87382d2f5d4bb1e69aab757a855ce87ea3cf
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-02-12 17:28:48 +0700

    phuong_check_login_to_homepage

--- SmartPhone/src/main/java/com/example/smartphone/controller/SceneController.java
+++ SmartPhone/src/main/java/com/example/smartphone/controller/SceneController.java
@@ -4,88 +4,69 @@ import dao.JDBCConnect;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.InputEvent;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ResourceBundle;
 
-public class SceneController implements Initializable {
+public class SceneController {
     @FXML
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
+    private TextField tfEmailLogin;
+    @FXML
+    private TextField tfUsernameLogin;
+    @FXML
+    private PasswordField tfPasswordLogin;
+    @FXML
+    private Button btnCon;
     @FXML
     private Label loginMessageLabel;
-    @FXML
-    public void SwitchToRegister(ActionEvent event) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            InputStream inputStream = getClass().getResourceAsStream("/com/example/smartphone/register.fxml");
-            if (inputStream == null) {
-                throw new IOException("FXML file not found");
-            }
-            root = fxmlLoader.load(inputStream);
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+    public SceneController(TextField tfEmailLogin) {
+        this.tfEmailLogin = tfEmailLogin;
     }
 
-
-
     @FXML
-    public void loginButtonOnAction(ActionEvent event) throws SQLException {
-        login();
-    }
-    @FXML
-    public TextField tfEmailLogin;
-
-    public TextField tfUsernameLogin;
-    public PasswordField tfPasswordLogin;
-    public Button btnCon;
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        btnCon.setOnAction(actionEvent -> {
+    public void initialize() {
+        btnCon.setOnAction(event -> {
             try {
                 login();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+            } catch (SQLException | IOException e) {
+                e.printStackTrace();
             }
         });
     }
+
     @FXML
-    public void login() throws SQLException {
+    public void SwitchToRegister(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/smartphone/register.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void login() throws SQLException, IOException {
+        Connection con = JDBCConnect.getJDBCConnection();
         PreparedStatement preparedStatement = null;
         ResultSet rs = null;
-        Connection con = JDBCConnect.getJDBCConnection();
+
         try {
-            preparedStatement = con.prepareStatement("SELECT * FROM  user WHERE email = ? AND  username = ? AND password = ?");
+            preparedStatement = con.prepareStatement("SELECT * FROM user WHERE email = ? AND username = ? AND password = ?");
             preparedStatement.setString(1, tfEmailLogin.getText());
             preparedStatement.setString(2, tfUsernameLogin.getText());
             preparedStatement.setString(3, tfPasswordLogin.getText());
             rs = preparedStatement.executeQuery();
             if (rs.next()) {
                 // Load the new FXML file
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/smartphone/home.fxml"));
-                Parent root = fxmlLoader.load();
+                Parent root = FXMLLoader.load(getClass().getResource("/com/example/smartphone/home.fxml"));
                 Scene scene = new Scene(root, 520, 400);
                 Stage stage = (Stage) tfUsernameLogin.getScene().getWindow();
                 stage.setScene(scene);
@@ -94,8 +75,6 @@ public class SceneController implements Initializable {
                 Alert alert = new Alert(Alert.AlertType.WARNING, "Login Failed !", ButtonType.OK);
                 alert.show();
             }
-        } catch (SQLException | IOException e) {
-            throw new RuntimeException(e);
         } finally {
             // Close resources
             if (rs != null) {
