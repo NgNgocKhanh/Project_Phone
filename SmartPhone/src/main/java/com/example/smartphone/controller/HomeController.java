a504b095df73f2e256b2f09c802d53feb9c09c75
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-02-14 19:58:49 +0700

    phuong_check_login_to_homepage

--- SmartPhone/src/main/java/com/example/smartphone/controller/HomeController.java
+++ SmartPhone/src/main/java/com/example/smartphone/controller/HomeController.java
@@ -7,10 +7,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.File;
 import java.io.IOException;
@@ -47,6 +50,28 @@ public class HomeController {
     private Button logOutButton;
 
     @FXML
+    void logoutClick(MouseEvent event) {
+        ButtonType resultConfirm = GetData.showConfirmationAlert("Confirmation message", "Are you sure you want to logout?");
+        // if user confirm delete then delete
+        if (resultConfirm.equals(ButtonType.OK)) {
+            // Close the main app window
+            Stage mainAppStage = (Stage) fullBorderPane.getScene().getWindow();
+            mainAppStage.close();
+
+            // Open the login window
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/demojavafxproject/login.fxml"));
+                Parent root = loader.load();
+
+                Stage loginStage = new Stage();
+                loginStage.setScene(new Scene(root));
+                loginStage.initStyle(StageStyle.TRANSPARENT);
+                loginStage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
     private void loadPage(String page) {
         Parent root = null;
         try {
@@ -62,6 +87,5 @@ public class HomeController {
             System.out.println("Cannot navigate page");
         }
         fullBorderPane.setCenter(root);
-    }
     }
 }
